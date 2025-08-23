de626b3734dc4bd58820b1bbb51a31a4aa13600b
Jaden Hampton <wearewebtastic@gmail.com>
2025-08-23 11:36:04 -0600

    contact form edit

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -115,7 +115,7 @@ export default function ContactForm() {
             />
 
             <div className="form-field">
-              <label htmlFor="Glass Type">Glass Type </label>
+              <label htmlFor="Glass Type">glass-type</label>
                 <select
                   id="glass-type"
                   defaultValue=""
